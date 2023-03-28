49d447e4cd6dc041bee304d8c791aa363753c26c
Mika RAMPARANY <migame.strass@gmail.com>
2023-03-28 16:09:58 +0200

    setting const and let

---
+++ script.js
@@ -0,0 +1,6 @@
+const boxs=document.querySelectorAll('.carre');
+const statusTxt=document.querySelector('#status');
+const btnRestart=document.querySelector('#restart');
+
+let x="<img src='images/X.png'>";
+let o="<img src='images/O.png'>";
